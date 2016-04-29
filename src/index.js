a48b18d322399f89f140c53279b07975528c09f4
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-29 11:13:14 -0400

    template

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from './components/App';
+//============my test===============
+
+
+render(<App name='World'/>, document.getElementById('root'));
